12dd5e268f6c5cca966988c78df37404c1d4aa83
Emil Boman <nat14ebo@student.lu.se>
2023-10-05 16:41:36 +0200

    Changed the name of var2 and var3.

--- main.cxx
+++ main.cxx
@@ -1,22 +1,22 @@
 #include <iostream>
 #include <string>
-int main(int numbargs, char *var1[]) {
+int main(int numbargs, char *argslist[]) {
   bool isIt3Args {numbargs==3} ;
   if (isIt3Args) {
-    std::string var2{var1[0]};
-    auto var3{*(var1[1])};
-    auto var4{var2.size()};
-    auto var5{std::atoi(var1[2])};
+    std::string exeName{argslist[0]};
+    auto FirstCharInArg1{*(argslist[1])};
+    auto var4{exeName.size()};
+    auto var5{std::atoi(argslist[2])};
     auto var6{0};
     auto var7{0};
-    std::string var8{var1[1]};
+    std::string var8{argslist[1]};
     while (true) {
       var6 += var8[var7++];
       if (var7 >= static_cast<int>(var8.size())) {
         break;
       }
     }
-    if ((var6 ^ var3 * 3) << (var4 & 0x1f) == var5) {
+    if ((var6 ^ FirstCharInArg1 * 3) << (var4 & 0x1f) == var5) {
       std::cout << "Correct!" << std::endl;
     } else {
       std::cout << "Wrong!" << std::endl;
