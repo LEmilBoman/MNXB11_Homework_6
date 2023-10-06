3f49242e705394eef052810c440b7ccecf1176e8
Emil Boman <nat14ebo@student.lu.se>
2023-10-06 13:25:40 +0200

    Replaced the while loop with a new function string_to_checksum. Changed the name of var6 to checksumArg1. Removed var7, since this variable, used to control the length of the removed while loop,  is no longer needed

--- main.cxx
+++ main.cxx
@@ -1,5 +1,12 @@
 #include <iostream>
 #include <string>
+int string_to_checksum(std::string s){
+  int res {0};
+  for (char c: s){
+    res += c;
+  }
+  return res;
+}
 int main(int numbargs, char *argslist[]) {
   bool isIt3Args {numbargs==3} ;
   if (isIt3Args) {
@@ -7,16 +14,11 @@ int main(int numbargs, char *argslist[]) {
     auto FirstCharInArg1{*(argslist[1])};
     auto exeNameSize{exeName.size()};
     int IntVerArg2{std::atoi(argslist[2])};
-    auto var6{0};
-    auto var7{0};
+
     std::string Arg1{argslist[1]};
-    while (true) {
-      var6 += Arg1[var7++];
-      if (var7 >= static_cast<int>(Arg1.size())) {
-        break;
-      }
-    }
-    if ((var6 ^ FirstCharInArg1 * 3) << (exeNameSize & 0x1f) == IntVerArg2) {
+    int checksumArg1 {string_to_checksum(Arg1)};
+    
+    if ((checksumArg1 ^ FirstCharInArg1 * 3) << (exeNameSize & 0x1f) == IntVerArg2) {
       std::cout << "Correct!" << std::endl;
     } else {
       std::cout << "Wrong!" << std::endl;
